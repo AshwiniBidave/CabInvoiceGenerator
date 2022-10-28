7c1a5a131519ff8c1b658fd63ceccaaec397c860
AshwiniBidave <ashwinibidave1993@gmail.com>
2022-10-28 12:38:51 +0530

    first commit

---
+++ CabInvoiceGenerator/src/main/java/com/bridgelabz/CabInvoiceGenrator.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+/**
+ * Hello world!
+ *
+ */
+public class CabInvoiceGenrator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
